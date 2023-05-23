2e0b65a3d513377bb8a540b42870b475a9d02f60
AdamKhalepo <adam.khalepo@protonmail.com>
2023-05-23 21:34:09 +0200

    first commit : project created

---
+++ src/main/java/com/miage/miageland_back/MiagelandBackApplication.java
@@ -0,0 +1,13 @@
+package com.miage.miageland_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiagelandBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiagelandBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miage/miageland_back/MiagelandBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miage.miageland_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiagelandBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
