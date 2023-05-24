d406d5558a18bf2106d5fad25701da767da2af70
AdamKhalepo <adam.khalepo@protonmail.com>
2023-05-24 15:22:43 +0200

    feat(data): added default data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO PARK (ID,JAUGE_MAX,NB_MAX_JOURNALIER) VALUES (1,1000,0);
+
+-- 0 = employee, 1 = admin, 2 = manager
+INSERT INTO EMPLOYEE (ID,FIRST_NAME,NAME,EMAIL,ROLE) VALUES (1,'Tchouk','Tchouk','tchoukdu12@gmail.com','2');
+INSERT INTO EMPLOYEE (ID,FIRST_NAME,NAME,EMAIL,ROLE) VALUES (2,'Adam','Khalepo','adam@gmail.com','0');
+INSERT INTO EMPLOYEE (ID,FIRST_NAME,NAME,EMAIL,ROLE) VALUES (3,'Lydia','Boulfrad','lydia@gmail.com','0');
+INSERT INTO EMPLOYEE (ID,FIRST_NAME,NAME,EMAIL,ROLE) VALUES (4,'Maneva','Rasolondraibe','maneva@gmail.com','1');
